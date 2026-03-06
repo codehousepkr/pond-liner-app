4140cc2627b074ee0ebe7344cdd02793f697a7d2
codehouse <codehousepkr@gmail.com>
2026-03-06 21:29:03 +0500

    fix: fresh migration init-fresh

--- prisma/migrations/20260306162750_init_fresh/migration.sql
+++ prisma/migrations/20260306162750_init_fresh/migration.sql
@@ -15,8 +15,6 @@ CREATE TABLE "Session" (
     "locale" TEXT,
     "collaborator" BOOLEAN DEFAULT false,
     "emailVerified" BOOLEAN DEFAULT false,
-    "refreshToken" TEXT,
-    "refreshTokenExpires" TIMESTAMP(3),
 
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
