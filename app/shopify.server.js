cdafe38eb2b52aeb1012d603ddea5153761106c2
codehouse <codehousepkr@gmail.com>
2026-03-06 05:52:15 +0500

    fix: restore embedded auth strategy

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -18,6 +18,9 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
+  future: {
+    unstable_newEmbeddedAuthStrategy: true,
+  },
 });
 
 export default shopify;
